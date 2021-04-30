052db5ddabbd4277e6ff3cd3616de88f29a47539
Nicolaas0 <nicolerrick@gmail.com>
2021-04-30 21:04:51 +0700

    create post component

---
+++ src/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'
+
+const Post = () => {
+    return (
+        <div className='post'>
+            <div className='container'>
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default Post ;

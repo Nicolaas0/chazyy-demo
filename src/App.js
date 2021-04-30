0c182e7772080471a2d3bfce1e2f87c104781b89
Nicolaas0 <nicolerrick@gmail.com>
2021-04-30 21:04:34 +0700

    add post component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './index.css'
 import Navbar from './Navbar';
+import Post from './Post';
 
 function App() {
   return (
-    <div className='nav'>
-      <Navbar/>
+    <div>
+      <div className='nav'>
+        <Navbar />
+      </div>
+      <div className='content'>
+        <Post/>
+      </div>
     </div>
   );
 }

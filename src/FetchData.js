9bdc3387882d9364b62b503713231a53d2ee572f
Nicolaas0 <nicolerrick@gmail.com>
2021-04-30 19:38:36 +0700

    create hooks for fetching data

--- src/FetchData.js
+++ src/FetchData.js
@@ -2,17 +2,36 @@ import { useState } from "react";
 
 const FetchData = () => {
     const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+
+        const abort = new AbortController();
+
+        fetch({url, signal: abort.signal})
             .then((res) => {
+                if (!res.ok) {
+                    throw Error('Error fetching data');
+                }
                 return res.json;
             })
             .then((data) => {
                 setData(data);
+                setLoading(false);
+                setError(null);
             })
+            .catch(() => {
+                if (err.name === 'AbortError') {
+                    console.log('Fetch Abort');
+                }
+                setLoading(false);
+                setError(err.message);
+            })
+        
+        return () => abort.abort();
     }, [url]);
-    return { data };
+    return { data, loading, error };
 }
  
 export default FetchData;

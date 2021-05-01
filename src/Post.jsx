506201ae7c3eb84ff9313d7bb7a4036e3bf32f4e
Nicolaas0 <nicolerrick@gmail.com>
2021-05-01 22:25:54 +0700

    creating basic post structure

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import './index.css'
+import React from "react";
+import "./index.css";
 
 const Post = () => {
-    return (
-        <div className='post'>
-            <div className='container'>
-                
-            </div>
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="input-group">
+          <label>Username</label>
+          <input type="text" />
         </div>
-    );
-}
- 
-export default Post ;
+        <div className="input-group">
+          <label>Password</label>
+          <input type="password" />
+        </div>
+        <button className="btn">Post</button>
+      </div>
+    </div>
+  );
+};
+
+export default Post;

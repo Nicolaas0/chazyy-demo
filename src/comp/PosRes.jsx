a95ad13028a0e11f8b06d03ac9a60c193ea8459a
Nicolaas0 <nicolerrick@gmail.com>
2021-05-02 19:27:00 +0700

    creating the structure of the component + mapping

--- src/comp/PosRes.jsx
+++ src/comp/PosRes.jsx
@@ -2,7 +2,15 @@ const PosRes = () => {
     return (
         <div className='pos-res'>
             <div className='container'>
-                
+                {
+                    data.map((d) => {
+                        <div className="data-prev">
+                            <div className="username">{d.username}</div>
+                            <p className="message">{d.message}</p>
+                            <div className="like">{d.like}</div>
+                        </div>
+                    })
+                }
             </div>
         </div>
     );

11d48a539425479bc3c4daef49b4f7c75010c7eb
Nicolaas0 <nicolerrick@gmail.com>
2021-05-02 21:22:16 +0700

    add styling and change tag

--- src/comp/PosRes.jsx
+++ src/comp/PosRes.jsx
@@ -1,21 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import '../index.css'
+
 const PosRes = (prop) => {
+
     const data = prop.val;
-    
+
     return (
-        <div className='pos-res'>
-            <div className='container'>
+            <Container>
                 {
-                    data.map((d) => {
-                        <div className="data-prev">
-                            <div className="username">{d.username}</div>
-                            <p className="message">{d.message}</p>
-                            <div className="like">{d.like}</div>
-                        </div>
-                    })
+                    data.map((d) => (
+                        <DataPrev key={d.id}>
+                            <Username>{d.username}</Username>
+                            <Message className="message">{d.message}</Message>
+                            <Like>Like: {d.like}</Like>
+                        </DataPrev>
+    ))
                 }
-            </div>
-        </div>
+            </Container>
     );
 }
+
+const DataPrev = styled.div`
+    margin-bottom:0.5rem;
+    padding:0;
+`
+
+const Username = styled.div`
+    color:blue;
+    font-size:0.8rem;
+`
+
+const Message = styled.p`
+    margin:0;
+    padding:0;
+`
+
+const Like = styled.div`
+    margin:0.5rem 0;
+    font-size:0.7rem
+`
+
+const Container = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:baseline;
+    align-items:center;
+`
+
+
  
 export default PosRes;

69151e8fd37f26a63bb458d53cee27b2bc7e4446
Nicolaas0 <nicolerrick@gmail.com>
2021-05-03 19:44:09 +0700

    navbar is done.. for now

--- src/comp/Navbar.jsx
+++ src/comp/Navbar.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
 import styled from 'styled-components';
 import '../index.css'
 
 const Navbar = () => {
     return (
         <Nav>
-                <Logo>Logo</Logo>
-                <Search/>
+                <Logo>Chazyy</Logo>
+                <Search placeholder='What are you looking for?'/>
                 <NavList>
-                    <a href="">Home</a>
-                    <a href="">About</a>
-                    <a href="">Contact</a>
+                    <Item href="">Home</Item>
+                    <Item href="">About</Item>
+                    <Item href="">Contact</Item>
                 </NavList>
         </Nav>
     );
@@ -18,10 +17,34 @@ const Navbar = () => {
 
 const Logo = styled.h2`
     margin:0;
+    cursor:pointer;
+
+    &:hover{
+        outline: solid 0.3rem ${props=> props.theme.mainYellow};
+    }
 `
 
 const Search = styled.input`
+    border:0.25rem ${props => props.theme.mainBlue} solid;
+    padding:0.25rem;
+    width:15%;
+    margin:0;
+    
+        &:focus::placeholder{
+        color:transparent;
+    }
+`
 
+const Item = styled.a`
+    margin:0 1rem;
+    text-decoration:none;
+    color:black;
+    font-size:1.1rem;
+
+    &:hover{
+        border-bottom: 0.2rem solid ${props=> props.theme.mainBlue};
+        padding-bottom:0.1rem;
+    }
 `
 
 const NavList = styled.ul`
@@ -34,6 +57,7 @@ const Nav = styled.header`
     flex-direction:row;
     justify-content:space-around;
     align-items:center;
+    border-bottom:0.3rem solid ${props=> props.theme.mainYellow};
 `
 
 export default Navbar;

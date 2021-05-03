84b2d0b8b77aed8d1a9d2865d7b46e80b02105f9
Nicolaas0 <nicolerrick@gmail.com>
2021-05-03 17:50:33 +0700

    importing theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import * as theme from './config/theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

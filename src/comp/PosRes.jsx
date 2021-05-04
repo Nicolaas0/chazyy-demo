83cc0ba64f02dfb4550b97ac0b790f17556d96b3
Nicolaas0 <nicolerrick@gmail.com>
2021-05-04 22:11:33 +0700

    done styling

--- src/comp/PosRes.jsx
+++ src/comp/PosRes.jsx
@@ -9,11 +9,11 @@ const PosRes = (prop) => {
     return (
             <Container>
                 {
-                    data.map((d) => (
+                data.map((d) => (
+
                         <DataPrev key={d.id}>
                             <Username>{d.username}</Username>
                             <Message className="message">{d.message}</Message>
-                            <Like>Like: {d.like}</Like>
                         </DataPrev>
     ))
                 }
@@ -22,18 +22,26 @@ const PosRes = (prop) => {
 }
 
 const DataPrev = styled.div`
-    margin-bottom:0.5rem;
     padding:0;
+    width:25rem;
+    height:auto;
+    padding:0.5rem;
+    margin:1rem 1.5rem;
+    border:0.35rem ${props => props.theme.mainBlue} solid;
 `
 
 const Username = styled.div`
     color:blue;
     font-size:0.8rem;
+    margin-left:0.5rem;
+    font-size:0.8rem;
+    padding:0;
 `
 
 const Message = styled.p`
-    margin:0;
+    margin:0.5rem 0;
     padding:0;
+    text-align:center;
 `
 
 const Like = styled.div`
@@ -43,11 +51,14 @@ const Like = styled.div`
 
 const Container = styled.div`
     display:flex;
+    width:30rem;
     flex-direction:column;
-    justify-content:baseline;
     align-items:center;
+    border:1px solid black;
+    border:0.35rem ${props => props.theme.mainYellow} solid;
+    height:30rem;
+    overflow:scroll;
+    scroll-behavior:smooth;
 `
-
-
  
 export default PosRes;
--- src/comp/Post.jsx
+++ src/comp/Post.jsx
@@ -1,29 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 
 const Post = () => {
 
+  const [ username, setUsername ] = useState('');
+  const [ message, setMessage ] = useState('');
+  const [ pending, setIsPending ] = useState(false);
+
+  const fixUname = (name) => {
+    return '@'+ name
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let v = { username, message};
+    const un = fixUname(v.username);
+    v.username = un;
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/post", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(v),
+    }).then(() => {
+      setIsPending(false);
+    });
+  };
+
+    useEffect(() => {
+    //add dependencies for l so it didnt create infinite loop
+  }, []);
+
   return (
     <Container>
+      <form onSubmit={handleSubmit}>
       <Block>
         <Label>@</Label>
-        <Input type='text' />
+          <Input type='text'
+            required
+            value={username}
+            onChange={(e)=> setUsername(e.target.value)}/>
       </Block>
       <Block>
       <Label>Message</Label>
-        <TextArea type='text' />
+        <TextArea type='text' rows='5' cols='30'
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}/>
         </Block>
-      <Button>Post</Button>
+        {!pending && <Button>Post</Button>}
+        {pending && <Button>Adding</Button>}
+        </form>
     </Container>
   );
 };
 
 const Container = styled.div`
-  width:30%;
+  width:30rem;
+  height:17rem;
   border:0.35rem ${props => props.theme.mainYellow} solid;
   position:relative;
-  top:1rem;
-  left:2.5rem;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-evenly;
+  align-items:center;
 `
 
 const Label = styled.label`
@@ -31,14 +72,13 @@ const Label = styled.label`
 `
 
 const Block = styled.div`
-  margin: 0.5rem 6rem;
   display:block;
 `
 
 const Input = styled.input`
-  margin:0.5rem 0;
   border:0.25rem ${props => props.theme.mainBlue} solid;
   transition:0.3s;
+  font-size:0.9rem;
 
   &:focus{
     padding:0.3rem;
@@ -46,16 +86,15 @@ const Input = styled.input`
 `
 
 const Button = styled.button`
-  margin-left:11rem;
-  margin-bottom:0.5rem;
   background-color:transparent;
   border:0.25rem ${props => props.theme.mainBlue} solid;
   padding:0.2rem 0.5rem;
   transition:0.5s;
   cursor:pointer;
+  width:5rem;
   
   &:hover{
-    padding:0.2rem 1rem;
+    width:6rem;
   }
 `
 

7818fca5a4b494798c026f2b90bd8073bbcd0f74
Nicolaas0 <nicolerrick@gmail.com>
2021-05-04 22:09:42 +0700

    create styling using styled component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import './index.css';
 import Navbar from './comp/Navbar';
 import Post from './comp/Post';
+import Result from './comp/Result';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div>
-      <div className='nav'>
+    <Container>
+      <Nav>
         <Navbar/>
-      </div>
-      <div className='content'>
-        <Post/>
-      </div>
-    </div>
+      </Nav>
+      <Content>
+        <Post />
+        <Result/>
+      </Content>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display:flex;
+  flex-direction:column;
+`
+
+const Nav = styled.div`
+
+`
+
+const Content = styled.div`
+  display:flex;
+  flex-direction:row;
+  justify-content:space-around;
+  margin-top:1rem;
+`
 
 export default App;

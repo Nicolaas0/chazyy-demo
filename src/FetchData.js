211d97ab14bd9ec24d8f4d71548307ec7af2f3cd
Nicolaas0 <nicolerrick@gmail.com>
2021-05-04 22:11:02 +0700

    delete setTimeout and hooks dependencies for url

--- src/FetchData.js
+++ src/FetchData.js
@@ -4,17 +4,17 @@ const FetchData = (url) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-
-        fetch(url)
+            fetch(url)
             .then((res) => {
                 return res.json();
             })
             .then((data) => {
                 setData(data);
                 console.log('Data ready!');
-            })
-    }, [url]);
-    return { data};
+            });
+
+    });
+    return {data};
 }
  
 export default FetchData;

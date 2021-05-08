f548f95480d6f1e4dcd85b5b62f98d35b91a8575
Nicolaas0 <nicolerrick@gmail.com>
2021-05-08 21:24:04 +0700

    create error handler in useEffect to show up the error message

--- src/FetchData.js
+++ src/FetchData.js
@@ -2,19 +2,34 @@ import { useEffect, useState } from "react";
 
 const FetchData = (url) => {
     const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-            fetch(url)
+
+        const abortCont = new AbortController();
+        
+            fetch(url,{signal:abortCont.signal})
             .then((res) => {
+                if (!res.ok) {
+                    throw Error('Error Fetching Data');
+                }
                 return res.json();
             })
             .then((data) => {
                 setData(data);
                 console.log('Data ready!');
-            });
-
+                setError(null);
+            })
+                .catch((err) => {
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch Abort');
+                    } else {
+                        setError(err.message)
+                    }
+                })
+    return () => abortCont.abort();
     });
-    return {data};
+    return {data,error};
 }
  
 export default FetchData;

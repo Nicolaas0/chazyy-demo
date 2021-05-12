e5c9b314dd2d1581c55598aed0936c1e96cb6231
Nicolaas0 <nicolerrick@gmail.com>
2021-05-12 20:20:16 +0700

    add null components to show if the data is null

--- src/comp/Result.jsx
+++ src/comp/Result.jsx
@@ -1,15 +1,17 @@
 import FetchData from "../FetchData";
 import PosRes from "./PosRes";
 import ErrorFetch from "./ErrorFetch";
+import NullData from "./NullData";
 
 const Result = () => {
 
-    const { data,error } = FetchData('http://localhost:8000/post');
+    const { data,error,useNull } = FetchData('http://localhost:8000/post');
 
     return (
         <div>
             {data && <PosRes val={data} />}
             {error && <ErrorFetch err={error} />}
+            {useNull && <NullData/>}
         </div>
     )
 }

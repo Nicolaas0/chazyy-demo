ab5a3f35268f6f2ce71afc264f3a531fc5d91375
Nicolaas0 <nicolerrick@gmail.com>
2021-06-01 23:40:32 +0700

    make the openinng and desc font more smaller

--- src/comp/Profile.jsx
+++ src/comp/Profile.jsx
@@ -1,61 +1,62 @@
-import React from 'react';
-import styled from 'styled-components';
-import gitlogo from '../assest/github.png';
-import twitterlogo from '../assest/twitter.png';
+import React from "react";
+import styled from "styled-components";
+import gitlogo from "../assest/github.png";
+import twitterlogo from "../assest/twitter.png";
 
 const Profile = () => {
+  const op = "Hello my Name is Albert Nicolas Augustinus Lerrick!";
+  const mess =
+    "Thanks for using this SPA, i creating this for pratice my knowledge about react. For the source code you can see it on my Github:)";
 
-    const op = 'Hello my Name is Albert Nicolas Augustinus Lerrick!';
-    const mess = 'Thanks for using this SPA, i creating this for pratice my knowledge about react. For the source code you can see it on my Github:)'
-
-    return (
-        <Container>
-            <Opening>{op}</Opening>
-            <Desc>{mess}</Desc>
-            <List>
-                <GithubLogo src={gitlogo}></GithubLogo>
-                <TwitterLogo src ={twitterlogo}></TwitterLogo>
-            </List>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Opening>{op}</Opening>
+      <Desc>{mess}</Desc>
+      <List>
+        <a href="https://github.com/Nicolaas0">
+          <GithubLogo src={gitlogo}></GithubLogo>
+        </a>
+        <TwitterLogo src={twitterlogo}></TwitterLogo>
+      </List>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-width:30rem;
-height:11rem;
-border:0.35rem ${props => props.theme.mainYellow} solid;
-position:relative;
-display:flex;
-flex-direction:column;
-justify-content:space-evenly;
-align-items:center;
-`
+  width: 30rem;
+  height: 11rem;
+  border: 0.35rem ${(props) => props.theme.mainYellow} solid;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 
 const GithubLogo = styled.img`
-    margin:0;
-    padding:0;
-    cursor:pointer;
-    Transform:scale(0.8);
-`
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+  transform: scale(0.8);
+`;
 
 const TwitterLogo = styled.img`
-    margin:0;
-    padding:0;
-    cursor:pointer;
-    Transform:scale(0.8);
-`
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+  transform: scale(0.8);
+`;
 
 const Opening = styled.div`
-    
-`
+  font-size: 0.85rem;
+`;
 
 const Desc = styled.p`
-text-align:center;
-margin:0;
-`
+  text-align: center;
+  margin: 0;
+  font-size: 0.80rem;
+`;
 
-const List = styled.div`
-    
-`
+const List = styled.div``;
 
 export default Profile;

89febb80ff35b5dbd4d86c6bd288dabd6bd76992
Nicolaas0 <nicolerrick@gmail.com>
2021-06-02 22:42:27 +0700

    adding color theme for the web

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,3 +1,5 @@
-export const mainBlue = '#003566';
-export const mainYellow = '#FFC300';
-export const warningRed = '#ff686b';
+export const bgColor = "#2a2a2a";
+export const lghtBgColor = "#474747";
+export const utilBlack = "#1B1B1B";
+export const fontCol = "#8E8E8E";
+export const themeColor = "#70FFA7";

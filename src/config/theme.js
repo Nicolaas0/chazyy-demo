7de759df1ac8ffece7a197d01bba9eccb3ec2f2f
Nicolaas0 <nicolerrick@gmail.com>
2021-06-03 20:20:23 +0700

    add lghtUtilBlack color theme

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,5 +1,6 @@
 export const bgColor = "#2a2a2a";
-export const lghtBgColor = "#474747";
+export const lghtBgColor = "#202020";
 export const utilBlack = "#1B1B1B";
+export const lghtUtilBlack = "#161616";
 export const fontCol = "#8E8E8E";
 export const themeColor = "#70FFA7";

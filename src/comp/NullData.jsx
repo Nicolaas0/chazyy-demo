beb97bf66c1784622a17312623d17ffc05787995
Nicolaas0 <nicolerrick@gmail.com>
2021-06-04 21:24:11 +0700

    changing the style like posres

--- src/comp/NullData.jsx
+++ src/comp/NullData.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import storm from "../assest/strom.svg";
 
 const NullData = () => {
-    return (
-                <Container>
-            <ErrorMes>Data is not available</ErrorMes>
-        </Container>
-    );
-}
- 
+  return (
+    <Container>
+      <Icon src={storm}></Icon>
+      <ErrorMes>Data is Unavailable</ErrorMes>
+    </Container>
+  );
+};
+
 const Container = styled.div`
-    display:flex;
-    width:30rem;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    border:1px solid black;
-    border:0.35rem ${props => props.theme.mainYellow} solid;
-    height:30rem;
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
+  width: 30rem;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 1rem;
+  border: 0.2rem solid ${(props) => props.theme.themeColor};
+  background-color: ${(props) => props.theme.bgColor};
+  height: 15rem;
+`;
+
+const Icon = styled.img`
+  width:90px;
 `
 
 const ErrorMes = styled.div`
-
-`
+  font-size: 1.5rem;
+  font-family: Archivo;
+  color: ${(props) => props.theme.lghtUtilBlack};
+`;
 
 export default NullData;

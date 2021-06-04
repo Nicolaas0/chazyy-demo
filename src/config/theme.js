60ae24b812a604a8c07a36ac1de29bf1cb79dd94
Nicolaas0 <nicolerrick@gmail.com>
2021-06-04 21:25:05 +0700

    add utilCol and change the value of lghtUtilBlack color

--- src/config/theme.js
+++ src/config/theme.js
@@ -2,5 +2,6 @@ export const bgColor = "#2a2a2a";
 export const lghtBgColor = "#202020";
 export const utilBlack = "#1B1B1B";
 export const lghtUtilBlack = "#161616";
-export const fontCol = "#8E8E8E";
+export const utilCol = "#ABABAB";
+export const fontCol = "#CDCDCD";
 export const themeColor = "#70FFA7";

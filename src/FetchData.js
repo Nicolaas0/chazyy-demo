58e9b18aa6d1a3fe27425ba1c06f74ea7cda265d
Nicolaas0 <nicolerrick@gmail.com>
2021-06-05 22:01:22 +0700

    put the data to an array and make it reverse to desceding the data

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,42 +1,43 @@
 import { useEffect, useState } from "react";
 
 const FetchData = (url) => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [useNull, setNull] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [useNull, setNull] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
+    const abortCont = new AbortController();
 
-        const abortCont = new AbortController();
-
-            fetch(url,{signal:abortCont.signal})
-            .then((res) => {
-                if (!res.ok) {
-                    throw Error('Error Fetching Data');
-                }
-                return res.json();
-            })
-                .then((data) => {
-                    if (data.length !== 0) {
-                    setData(data);
-                    setError(null);
-                    } else {
-                        setData(null);
-                        setNull(true);
-                        setError(null);
-                        console.log('Data is unavailable');
-                    }
-            })
-                .catch((err) => {
-                    if (err.name === 'AbortError') {
-                        console.log('Fetch Abort');
-                    } else {
-                        setError(err.message)
-                    }
-                })
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Error Fetching Data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.length !== 0) {
+          const arr = [];
+          arr.push(data);
+          setData(arr[0].reverse());
+          setError(null);
+        } else {
+          setData(null);
+          setNull(true);
+          setError(null);
+          console.log("Data is unavailable");
+        }
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("Fetch Abort");
+        } else {
+          setError(err.message);
+        }
+      });
     return () => abortCont.abort();
-    },[data,url]);
-    return {data,error,useNull};
-}
- 
+  }, [data, url]);
+  return { data, error, useNull };
+};
+
 export default FetchData;

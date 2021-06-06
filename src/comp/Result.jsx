eb7bc2cb53e1135abfd5c8d4ac34fef4c7108005
Nicolaas0 <nicolerrick@gmail.com>
2021-06-06 21:19:47 +0700

    add conditional rendering for loading data

--- src/comp/Result.jsx
+++ src/comp/Result.jsx
@@ -2,16 +2,18 @@ import FetchData from "../FetchData";
 import PosRes from "./PosRes";
 import ErrorFetch from "./ErrorFetch";
 import NullData from "./NullData";
+import Loading from "./Loading";
 
 const Result = () => {
 
-    const { data,error,useNull } = FetchData('http://localhost:8000/post');
+    const { data,error,useNull, loading } = FetchData('http://localhost:8000/post');
 
     return (
         <div>
             {data && <PosRes val={data} />}
             {error && <ErrorFetch err={error} />}
             {useNull && <NullData/>}
+            {loading && <Loading/>}
         </div>
     )
 }

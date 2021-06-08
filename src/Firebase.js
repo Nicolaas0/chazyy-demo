51d63cfdad5830ed8331625f3107ce8fc08ef6d6
Nicolaas0 <nicolerrick@gmail.com>
2021-06-08 20:18:40 +0700

    firebase config file

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCaJlXubJpyMsiAJKHaHGz6hfkIN7MDFC0",
+  authDomain: "chazyy-e64d1.firebaseapp.com",
+  projectId: "chazyy-e64d1",
+  storageBucket: "chazyy-e64d1.appspot.com",
+  messagingSenderId: "559137273140",
+  appId: "1:559137273140:web:ff6d1460af4a6a585a6161",
+  measurementId: "G-RGCFRG0DH0",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase

ff5eee49fbaf01d5eb88b345d1b499483892b240
Nicolaas0 <nicolerrick@gmail.com>
2021-06-09 19:40:00 +0700

    change styling to match the other comp

--- src/comp/ErrorFetch.jsx
+++ src/comp/ErrorFetch.jsx
@@ -1,53 +1,66 @@
-import React from 'react';
-import styled from 'styled-components';
-import Result from '../comp/Result';
+import React from "react";
+import styled from "styled-components";
+import Result from "../comp/Result";
+import storm from '../assest/strom.svg';
 
 const ErrorFetch = (props) => {
+  const mess = props.err;
 
-    const mess = props.err;
-    
-    const handleReload = () => {
-        const url = Result;
-        window.location.href = url;
-    }
+  const handleReload = () => {
+    const url = Result;
+    window.location.href = url;
+  };
 
-    return (
-        <Container>
-            <ErrorMes>{mess}</ErrorMes>
-            <Refresh onClick={handleReload}>Refresh</Refresh>
-        </Container>
-    );
-}
- 
+  return (
+    <Container>
+      <Icon src={storm}></Icon>
+      <ErrorMes>{mess}</ErrorMes>
+      <Refresh onClick={handleReload}>Refresh</Refresh>
+    </Container>
+  );
+};
+
+const Icon = styled.img`
+  width: 90px;
+`;
+
 const Container = styled.div`
-    display:flex;
-    width:30rem;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    border:1px solid black;
-    border:0.35rem ${props => props.theme.mainYellow} solid;
-    height:30rem;
-`
-
+  margin-bottom: 1rem;
+  display: flex;
+  width: 30rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1rem;
+  border: 0.2rem solid ${(props) => props.theme.utilBlack};
+  background-color: ${(props) => props.theme.bgColor};
+  height: 15rem;
+`;
 
 const ErrorMes = styled.div`
-    
-`
+  color: ${(props) => props.theme.fontCol};
+  font-family: Archivo;
+  letter-spacing: 0.05rem;
+`;
 
 const Refresh = styled.button`
-margin-top:1rem;
-      background-color:transparent;
-  border:0.25rem ${props => props.theme.mainBlue} solid;
-  padding:0.2rem 0.5rem;
-  transition:0.5s;
-  cursor:pointer;
-  width:5rem;
-  
-  &:hover{
-    width:6rem;
-      border-radius:5rem;
+  font-family: Archivo;
+  letter-spacing: 0.05rem;
+  margin-top: 1rem;
+  background-color: transparent;
+  color: ${(props) => props.theme.fontCol};
+  border: 0.25rem ${(props) => props.theme.lghtUtilBlack} solid;
+  padding: 0.2rem 0.5rem;
+  transition: 0.5s;
+  cursor: pointer;
+  width: 5rem;
+
+  &:hover {
+    width: 6rem;
+    border-radius: 5rem;
+    color: ${(props) => props.theme.themeColor};
+    border-color: ${(props) => props.theme.themeColor};
   }
-`
+`;
 
 export default ErrorFetch;

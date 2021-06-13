6ab4dc85b5c2314ce957394f74ec36b823a1a78f
Nicolaas0 <nicolerrick@gmail.com>
2021-06-13 23:17:44 +0700

    white space commit & add moment package

--- src/FetchData.js
+++ src/FetchData.js
@@ -7,7 +7,6 @@ const FetchData = (ep) => {
   const [useNull, setNull] = useState(false);
   const [loading, setLoading] = useState(true);
 
-
   const showData = (ep) => {
     const ref = firebase.database().ref(ep);
     ref.on("value", (snapshot) => {

48c366dc391c4466f577c951d7e517dde38d3093
Nicolaas0 <nicolerrick@gmail.com>
2021-06-16 22:29:41 +0700

    change the all laptop value to min-width

--- src/config/breakp.js
+++ src/config/breakp.js
@@ -13,8 +13,8 @@ export const dev = {
   mobileM: `(max-width: ${size.mobileM})`,
   mobileL: `(max-width: ${size.mobileL})`,
   tablet: `(max-width: ${size.tablet})`,
-  laptop: `(max-width: ${size.laptop})`,
-  laptopL: `(max-width: ${size.laptopL})`,
-  desktop: `(max-width: ${size.desktop})`,
-  desktopL: `(max-width: ${size.desktop})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`,
 };

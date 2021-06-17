daee051baf9434178af619b9dfc881dfbfc6c34c
Nicolaas0 <nicolerrick@gmail.com>
2021-06-17 22:11:53 +0700

    add footer component

---
+++ src/comp/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <Container>
+      <a target="_blank" href="https://icons8.com/icon/DxIsF9smUsRE/heart">
+        Heart
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+
+export default Footer;

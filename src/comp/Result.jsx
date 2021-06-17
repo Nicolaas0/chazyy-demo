12ce731cc0d1558e9e387fbe2257a70e8dd12381
Nicolaas0 <nicolerrick@gmail.com>
2021-06-17 20:52:15 +0700

    comment the firebase import

--- src/comp/Result.jsx
+++ src/comp/Result.jsx
@@ -1,19 +1,17 @@
 import FetchData from "../FetchData";
 import PosRes from "./PosRes";
 import ErrorFetch from "./ErrorFetch";
-import NullData from "./NullData";
 import Loading from "./Loading";
-import firebase from '../Firebase'
+// import firebase from '../Firebase'
 
 const Result = () => { 
     // const ref = firebase.database().ref('Demo/');
-    const { data, error, useNull, loading } = FetchData('Demo');
+    const { data, error, loading } = FetchData('Demo');
 
     return (
         <div>
             {data && <PosRes val={data} />}
             {error && <ErrorFetch err={error} />}
-            {useNull && <NullData/>}
             {loading && <Loading/>}
         </div>
     )

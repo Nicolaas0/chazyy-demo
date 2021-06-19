19d47c503e88977d99492c1d18750e1773ee5c3a
Nicolaas0 <nicolerrick@gmail.com>
2021-06-19 22:32:06 +0700

    change website title and add footer link for free license

--- src/comp/Footer.jsx
+++ src/comp/Footer.jsx
@@ -1,20 +1,68 @@
 import React from "react";
 import styled from "styled-components";
+import { dev } from '../config/breakp'
 
 const Footer = () => {
   return (
     <Container>
-      <a target="_blank" href="https://icons8.com/icon/DxIsF9smUsRE/heart">
-        Heart
-      </a>{" "}
-      icon by{" "}
-      <a target="_blank" href="https://icons8.com">
-        Icons8
-      </a>
+      <Link>
+        <Icon target="_blank" href="https://icons8.com/icon/DxIsF9smUsRE/heart">
+          Heart
+        </Icon>{" "}
+        icon by{" "}
+        <Web target="_blank" href="https://icons8.com">
+          Icons8
+        </Web>
+      </Link>
+      <Link>
+        <Icon
+          target="_blank"
+          href="https://icons8.com/icon/IZss1rnAb_BA/stormtrooper"
+        >
+          Stormtrooper
+        </Icon>{" "}
+        icon by{" "}
+        <Web target="_blank" href="https://icons8.com">
+          Icons8
+        </Web>
+      </Link>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+width:100%;
+height:100%;
+display:flex;
+flex-direction:row;
+align-items:center;
+justify-content:center;
+
+  @media ${dev.mobileM} {
+    flex-direction:column;
+  }
+`;
+
+const Link = styled.div`
+  border-radius: 1rem;
+  text-align: center;
+  color: ${(props) => props.theme.utilCol};
+  width: auto;
+  height: auto;
+  margin: 0.5rem;
+  background-color: ${(props) => props.theme.lghtBgColor};
+  padding: 0.8rem;
+`;
+
+const Icon = styled.a`
+  color: ${(props) => props.theme.themeColor};
+  text-decoration: none;
+  font-weight:bold;
+`;
+
+const Web = styled.a`
+  color: ${(props) => props.theme.themeColor};
+  font-weight: bold;
+`;
 
 export default Footer;

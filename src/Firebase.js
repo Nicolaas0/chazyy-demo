9155fd5d18fb16a564f9bf5ac1b878ca3e436adf
Nicolaas0 <nicolerrick@gmail.com>
2021-06-20 21:34:33 +0700

    add proper import of firebase package

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional

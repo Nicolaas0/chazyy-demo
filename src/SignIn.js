995a5fe2d269145d3370bf2613610f8bd4fae12a
Nicolaas0 <nicolerrick@gmail.com>
2021-06-25 22:37:42 +0700

    fixing the sign in code

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,29 +1,41 @@
 import { useEffect, useState } from 'react';
 import firebase from './Firebase'
-const SignIn = () => {
 
-    const [useUid, setUid] = useState('')
-    
-      useEffect(() => {
+const SignIn = (email,pass) => {
+  // const [useUid, setUid] = useState('');
+
+  useEffect(() => {
         firebase
           .auth()
-          .signInAnonymously()
-          .then((user) => {
-
+          .createUserWithEmailAndPassword(email, pass)
+          .then((userCredential) => {
+            var user = userCredential.user;
+            console.log('Sucess!!')
+            console.log(user);
           })
-          .catch((error) => {
-            // var errorCode = error.code;
-            // var errorMess = error.message;
-          });
+          .catch(() => {});
+  },[])
+    
+      // useEffect(() => {
+      //   firebase
+      //     .auth()
+      //     .signInAnonymously()
+      //     .then((user) => {
 
-        firebase.auth().onAuthStateChanged((user) => {
-          if (user) {
-              var uid = user.uid;
-              setUid(uid)
-            console.log(uid);
-          } else {}
-        });
-      }, []);
+      //     })
+      //     .catch((error) => {
+      //       // var errorCode = error.code;
+      //       // var errorMess = error.message;
+      //     });
+
+      //   firebase.auth().onAuthStateChanged((user) => {
+      //     if (user) {
+      //         var uid = user.uid;
+      //         setUid(uid)
+      //       console.log(uid);
+      //     } else {}
+      //   });
+      // }, []);
     // return { useUid };
 }
 

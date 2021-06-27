088113637cbc20e6a78e14d0e2890bb3b44ba24c
Nicolaas0 <nicolerrick@gmail.com>
2021-06-27 23:01:12 +0700

    fixing sign in bug

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,41 +1,36 @@
-import { useEffect, useState } from 'react';
-import firebase from './Firebase'
+import { useEffect, useState } from "react";
+import firebase from "./Firebase";
 
-const SignIn = (email,pass) => {
+const SignIn = (email, pass) => {
   // const [useUid, setUid] = useState('');
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, pass)
+    .then(() => {
+      console.log('Sucess')
+    });
 
-  useEffect(() => {
-        firebase
-          .auth()
-          .createUserWithEmailAndPassword(email, pass)
-          .then((userCredential) => {
-            var user = userCredential.user;
-            console.log('Sucess!!')
-          })
-          .catch(() => {});
-  })
-    
-      // useEffect(() => {
-      //   firebase
-      //     .auth()
-      //     .signInAnonymously()
-      //     .then((user) => {
+  // useEffect(() => {
+  //   firebase
+  //     .auth()
+  //     .signInAnonymously()
+  //     .then((user) => {
 
-      //     })
-      //     .catch((error) => {
-      //       // var errorCode = error.code;
-      //       // var errorMess = error.message;
-      //     });
+  //     })
+  //     .catch((error) => {
+  //       // var errorCode = error.code;
+  //       // var errorMess = error.message;
+  //     });
 
-      //   firebase.auth().onAuthStateChanged((user) => {
-      //     if (user) {
-      //         var uid = user.uid;
-      //         setUid(uid)
-      //       console.log(uid);
-      //     } else {}
-      //   });
-      // }, []);
-    // return { useUid };
-}
+  //   firebase.auth().onAuthStateChanged((user) => {
+  //     if (user) {
+  //         var uid = user.uid;
+  //         setUid(uid)
+  //       console.log(uid);
+  //     } else {}
+  //   });
+  // }, []);
+  // return { useUid };
+};
 
 export default SignIn;

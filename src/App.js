74f79455a8c5afd4843fe4cdb1ff11a4f86d23dc
Nicolaas0 <nicolerrick@gmail.com>
2021-07-20 22:15:51 +0700

    moving themeProvider to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import SignInPage from "./comp/SignInPage";
 import SignUpPage from "./comp/SignUpPage";
 import ChazyyMain from './comp/ChazyyMain';
 import { AuthProvider } from "./comp/context/AuthContext";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import PrivateRoute from "./PrivateRoute";
+import { ThemeProvider } from "styled-components";
+import * as theme from "./config/theme";
 // import logo from './assest/logo.png'
 // import Profile from './comp/Profile';
 // import Loading from './comp/Loading';
@@ -14,19 +17,22 @@ function App() {
 
   return (
     <Router>
-      <AuthProvider>
-        <>
-          <Helmet>
-            <title>Chazyy | Demo</title>
-            {/* <link rel="icon" type="image/png" href={logo} /> */}
-          </Helmet>
-          <Switch>
-            <Route exact path="/" component={ChazyyMain}/>
-            <Route path="/SignIn" component={SignInPage}/>
-            <Route path="/SignUp" component={SignUpPage}/>
-          </Switch>
-        </>
-      </AuthProvider>
+      {/* <AuthProvider> */}
+        <ThemeProvider theme={theme}>
+          <>
+            <Helmet>
+              <title>Chazyy | Demo</title>
+              {/* <link rel="icon" type="image/png" href={logo} /> */}
+            </Helmet>
+            <Switch>
+              <Route exact path="/" component={ChazyyMain} />
+              <PrivateRoute exact path="/private" component={ChazyyMain} />
+              <Route path="/SignIn" component={SignInPage} />
+              <Route path="/SignUp" component={SignUpPage} />
+            </Switch>
+          </>
+        </ThemeProvider>
+      {/* </AuthProvider> */}
     </Router>
   );
 }

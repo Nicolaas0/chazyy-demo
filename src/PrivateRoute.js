daac39da80ea49019e3a2a89b43c6c74c268a641
Nicolaas0 <nicolerrick@gmail.com>
2021-07-20 22:15:28 +0700

    commenting all code, not using this code, yet.

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { useAuth } from "./comp/context/AuthContext";
-import { Route, Redirect } from "react-router-dom";
+// import React from "react";
+// import { useAuth } from "./comp/context/AuthContext";
+// import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { currentUser } = useAuth();
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        currentUser ? <Component {...props} /> : <Redirect to="/SignIn" />;
-      }}
-    ></Route>
-  );
-};
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//   const { currentUser } = useAuth();
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) => {
+//        return currentUser ? <Component {...props} /> : <Redirect to="/SignIn" />;
+//       }}
+//     ></Route>
+//   );
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;

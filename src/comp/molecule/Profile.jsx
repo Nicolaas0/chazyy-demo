2f3134e1eb72eaba4772691e3d3f360147442ed8
Nicolaas0 <nicolerrick@gmail.com>
2021-08-11 22:47:22 +0700

    change the entire file. reusing the file

--- src/comp/molecule/Profile.jsx
+++ src/comp/molecule/Profile.jsx
@@ -1,62 +1,195 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import gitlogo from "../assest/github.png";
-import twitterlogo from "../assest/twitter.png";
+import { dev } from "../../config/breakp";
+import { color } from "../../config/theme";
+import { useAuth } from "../context/AuthContext";
 
 const Profile = () => {
-  const op = "Hello my Name is Albert Nicolas Augustinus Lerrick!";
-  const mess =
-    "Thanks for using this SPA, i creating this for pratice my knowledge about react. For the source code you can see it on my Github:)";
+  const [error, setError] = useState(false);
+  const emailRef = useRef();
 
+  const { currentUser } = useAuth();
+  const { updateEmail } = useAuth();
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      if (emailRef.current.value === currentUser.email) {
+        setError(true);
+      } else {
+        await updateEmail(emailRef.current.value);
+        window.location.reload();
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {}, [currentUser.email]);
   return (
-    <Container>
-      <Opening>{op}</Opening>
-      <Desc>{mess}</Desc>
-      <List>
-        <a href="https://github.com/Nicolaas0">
-          <GithubLogo src={gitlogo}></GithubLogo>
-        </a>
-        <TwitterLogo src={twitterlogo}></TwitterLogo>
-      </List>
-    </Container>
+    <Body>
+      <Routing>
+        Chazyy /{" "}
+        <Link to="/Chazyy/">
+          <Redir>Main</Redir>
+        </Link>{" "}
+        / <Span>Profile</Span>
+      </Routing>
+      <MainCon onSubmit={handleUpdate}>
+        <Heading>Hello,</Heading>
+        <Email>{currentUser.email}</Email>
+        <SecHeading>wanna change your email?</SecHeading>
+        <Input placeholder="sure its....." required ref={emailRef} />
+        {error ? (
+          <Button>Error while updating</Button>
+        ) : (
+          <Button>Change it please...</Button>
+        )}
+      </MainCon>
+    </Body>
   );
 };
 
-const Container = styled.div`
-  width: 30rem;
-  height: 11rem;
-  border: 0.35rem ${(props) => props.theme.mainYellow} solid;
-  position: relative;
+const Body = styled.body`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
-`;
-
-const GithubLogo = styled.img`
   margin: 0;
   padding: 0;
+`;
+
+const Routing = styled.div`
+  margin: 1rem 0;
+  font-family: "Roboto Mono";
+  font-size: 1.4rem;
+  padding: 1rem;
+  margin: 1rem 0;
+  font-weight: 700;
+  color: ${color.lightgray};
+
+  @media${dev.tablet} {
+    font-size: 5vw;
+  }
+`;
+
+const MainCon = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+  width: 40vw;
+
+  @media${dev.tablet} {
+    width: 70vw;
+  }
+`;
+
+const Heading = styled.div`
+  margin: 2.5rem 0;
+  align-self: start;
+  height: auto;
+  width: auto;
+  font-size: 4.5vw;
+  font-family: "Roboto Mono";
+  color: ${color.lightgray};
+  font-weight: 400;
+
+  @media${dev.tablet} {
+    font-size:6vw;
+  }
+`;
+
+const SecHeading = styled.div`
+  margin: 2rem 0;
+  height: auto;
+  width: auto;
+  font-size: 1.7vw;
+  font-family: "Roboto Mono";
+  color: ${color.lightgray};
+  font-weight: 400;
+
+  @media${dev.tablet} {
+    font-size: 3.5vw;
+  }
+`;
+
+const Email = styled.div`
+  font-family: "Roboto Mono";
+  font-size: 3vw;
+  font-weight: 600;
+  text-decoration: underline;
+  color: ${color.lightgray};
+  height: auto;
+  width: auto;
+  transition: 0.5s;
   cursor: pointer;
-  transform: scale(0.8);
+
+  @media${dev.tablet} {
+    font-size: 6vw;
+  }
+
+  &:hover {
+    color: ${color.themecol};
+  }
 `;
 
-const TwitterLogo = styled.img`
-  margin: 0;
-  padding: 0;
+const Input = styled.input`
+  height: auto;
+  width: 20vw;
+  font-family: "Roboto Mono";
+  background-color: ${color.bgColor};
+  border: none;
+  font-size: 1.5vw;
+  border-bottom: 1px solid ${color.erie};
+  color: ${color.lightgray};
+  transition: 0.5s;
+
+  @media${dev.tablet} {
+    width:50vw;
+    font-size: 3vw;
+  }
+
+  &:focus {
+    border-color: ${color.themecol};
+  }
+`;
+
+const Button = styled.button`
+  margin: 1rem 0 0 0;
+  height: auto;
+  width: auto;
+  border: 2px solid ${color.erie};
+  background-color: ${color.bgColor};
+  color: ${color.sonicSv};
+  border-radius: 10px;
+  padding: 0.3rem;
+  font-family: "Roboto Mono";
   cursor: pointer;
-  transform: scale(0.8);
+  transition: 0.5s;
+
+  @media${dev.tablet} {
+    font-size: 3vw;
+  }
+
+  &:hover {
+    padding: 0.5rem 0.5rem;
+    color: ${color.themecol};
+    border-color: ${color.themecol};
+  }
 `;
 
-const Opening = styled.div`
-  font-size: 0.85rem;
+const Redir = styled.span`
+  font-family: "Roboto Mono";
+  color: ${color.lightgray};
 `;
 
-const Desc = styled.p`
-  text-align: center;
-  margin: 0;
-  font-size: 0.80rem;
+const Span = styled.span`
+  font-family: "Roboto Mono";
+  border-radius: 0.3rem;
+  background-color: ${color.erie};
+  padding: 0.1rem 0.2rem;
 `;
-
-const List = styled.div``;
-
 export default Profile;

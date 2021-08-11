0b2dfe5fdbe471c5baa9bc32aaa1b9533205dc83
Nicolaas0 <nicolerrick@gmail.com>
2021-08-11 22:47:31 +0700

    add light theme

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,17 +1,25 @@
+/* eslint-disable no-unused-vars */
 const BgColor = "#0A0A0A"; //FOR BACKGROUND ONLY! 
+const lBgColor = "#FFFFFF";
 
  // For Containers / Components / To make it different from the background * BRIGHTNESS VER OF THE BGCOLOR
 const RBFOGRA = "#121212";
+const lRBF = "#DDDDDD";
+
 const RBFOGRAp = "#050505";
+const lRBFp = "#B8B8B8";
 
 // For Inputs and Child Components
 const Erie = "#282828";
+const dErie = "#171717";
 
 // For Main Fonts! / body fonts
 const LightGray = "#CDCDCD";
+const dLG = "#1D1D1D";
 
 // For Description / Not important Text
 const SonicSv = "#787878";
+const dSSv = "#333333";
 
 // Logo main Color
 const SEC = "#00FFBB"; //SEC stands for Sea Green Crayola
@@ -24,14 +32,40 @@ const Warning = "#E91616";
 const WarningBg = "#EE4848";
 
 export const color = {
-    bgColor: BgColor,
-    rBFograp: RBFOGRAp,
-    rBFogra: RBFOGRA,
-    erie: Erie,
-    lightgray: LightGray,
-    platinum:Platinum,
-    sonicSv: SonicSv,
-    themecol: SEC,
-    warning: Warning,
-    warningbg: WarningBg
-}
+  bgColor: BgColor,
+  rBFograp: RBFOGRAp,
+  rBFogra: RBFOGRA,
+  erie: Erie,
+  lightgray: LightGray,
+  platinum: Platinum,
+  sonicSv: SonicSv,
+  themecol: SEC,
+  warning: Warning,
+  warningbg: WarningBg,
+};
+
+// export const darkMode = {
+//     bgColor: BgColor,
+//     rBFograp: RBFOGRAp,
+//     rBFogra: RBFOGRA,
+//     erie: Erie,
+//     lightgray: LightGray,
+//     platinum:Platinum,
+//     sonicSv: SonicSv,
+//     themecol: SEC,
+//     warning: Warning,
+//     warningbg: WarningBg
+// }
+
+// export const lightMode = {
+//   bgColor: lBgColor,
+//   rBFograp: lRBF,
+//   rBFogra: lRBFp,
+//   erie: dErie,
+//   lightgray: dLG,
+//   platinum: Platinum,
+//   sonicSv: dSSv,
+//   themecol: SEC,
+//   warning: Warning,
+//   warningbg: WarningBg,
+// };

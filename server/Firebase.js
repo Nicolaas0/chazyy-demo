830c1bd179588410bdee622f6f01f4c1d21793d1
Nicolaas0 <nicolerrick@gmail.com>
2021-10-21 16:25:52 +0700

    create firebase file that contains and export firebaseConfig

---
+++ server/Firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaJlXubJpyMsiAJKHaHGz6hfkIN7MDFC0",
+  authDomain: "chazyy-e64d1.firebaseapp.com",
+  databaseURL: "https://chazyy-e64d1-default-rtdb.firebaseio.com",
+  projectId: "chazyy-e64d1",
+  storageBucket: "chazyy-e64d1.appspot.com",
+  messagingSenderId: "559137273140",
+  appId: "1:559137273140:web:ff6d1460af4a6a585a6161",
+  measurementId: "G-RGCFRG0DH0",
+};
+// Initialize Firebase
+// initializeApp(firebaseConfig);
+
+// export default firebase;

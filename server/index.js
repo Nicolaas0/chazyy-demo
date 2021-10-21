78dd92ac6c1826088a349a3e82db0617f99c829a
Nicolaas0 <nicolerrick@gmail.com>
2021-10-21 16:24:34 +0700

    add some configuration and add user route

--- server/index.js
+++ server/index.js
@@ -3,14 +3,17 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { json, urlencoded } from "express";
 import chazyyRouter from "./router/chazyy-router.js";
+import userRouter from "./router/user-router.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(json({ limit: "30", extended: true }));
-app.use(urlencoded({ limit: "30", extended: true }));
+process.on("warning", (e) => console.warn(e.stack));
+app.use(json());
+app.use(urlencoded());
 
 app.use("/", chazyyRouter);
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`YOU ARE ON PORT ${PORT}`);

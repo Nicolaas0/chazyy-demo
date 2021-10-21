86788f6777ece9cdbd5fa8fef03f23dc1aed8b78
Nicolaas0 <nicolerrick@gmail.com>
2021-10-21 16:21:27 +0700

    creating get and post function for router

--- server/controllers/chazyy-cont.js
+++ server/controllers/chazyy-cont.js
@@ -1,3 +1,68 @@
-export const getChazyy = (req, res) => {
-    res.send('NICE!')
-}
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../Firebase.js";
+import dayjs from "dayjs";
+import uniqid from "uniqid";
+import {
+  collection,
+  addDoc,
+  getFirestore,
+  setDoc,
+  doc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+const date = dayjs().format("dddd, MMMM D YYYY");
+const id = uniqid();
+
+export const getChazyy = async (req, res) => {
+  const { username } = req.params;
+  const data = {
+    uid: null,
+    email: null,
+  };
+  const postData = [];
+
+  onAuthStateChanged(auth, (user) => {
+    console.log("ini si user yang lagi login", "=>", user);
+  });
+
+  const querySnapshot = await getDocs(collection(db, "chazyypost"));
+  querySnapshot.forEach((doc) => {
+    postData.push(doc.data());
+  });
+  data.data = postData;
+  console.log(data);
+};
+
+export const postChazyy = async (req, res) => {
+  const { username } = req.params;
+  const { message } = req.body.post;
+
+  const dbRef = doc(db, `chazyypost/${id}`);
+  const userRef = doc(db, `users/${username}/post/${id}`);
+
+  try {
+    await setDoc(dbRef, {
+      message: message,
+      dateCreated: date,
+      username: username,
+    });
+    await setDoc(userRef, {
+      message: message,
+      dateCreated: date,
+    });
+    console.log("Success posting data, the id: " + id);
+  } catch (e) {
+    console.log("Error posting data :" + e);
+  }
+};

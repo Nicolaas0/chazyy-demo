289f7622f6ee49341e4742652b9dd6ba57f0fe5a
Nicolaas <nicolerrick@gmail.com>
2021-12-22 20:39:49 +0700

    Fixing signin, signup, and change email function

--- src/comp/context/AuthContext.js
+++ src/comp/context/AuthContext.js
@@ -1,8 +1,12 @@
 import React, { useContext, useState, useEffect } from "react";
 import firebase from "../../Firebase";
+import dayjs from "dayjs";
+
 
 const AuthContext = React.createContext();
 const auth = firebase.auth();
+const firestore = firebase.firestore();
+const date = dayjs().format("DD/MM/YYYY HH:mm:ss");
 
 export const useAuth = () => {
   return useContext(AuthContext);
@@ -12,18 +16,29 @@ export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState("");
   const [loading, setLoading] = useState(true);
 
-  const signUp = (email, pass) => {
-    return auth.createUserWithEmailAndPassword(email, pass);
+  const signUp = async (email, pass) => {
+    await auth.createUserWithEmailAndPassword(email, pass)
+
+    await auth.onAuthStateChanged(user => {
+      firestore.collection('users').doc(user.uid).set({
+        dateCreated: date,
+      }).then(() => {
+        firestore.collection('users').doc(user.uid).collection('info').doc('accountInfo').set({
+          email: email,
+          pass: pass
+        })
+      })
+    })
   };
 
   const signIn = (email, pass) => {
     return auth.signInWithEmailAndPassword(email, pass);
   };
 
-    const updateEmail = (email) => {
-      return auth.currentUser.updateEmail(email);
-    };
-  
+  const updateEmail = (email) => {
+    return auth.currentUser.updateEmail(email);
+  };
+
   // eslint-disable-next-line no-unused-vars
   const logout = () => {
     return auth.signOut()
@@ -37,7 +52,7 @@ export const AuthProvider = ({ children }) => {
     });
 
     return unsubscribe;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const value = {
